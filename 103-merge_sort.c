a50e5547a05893aba1f9bcd4bed90c0df3381599
Patrick christopher <141264544+darlingdiamond01@users.noreply.github.com>
2024-01-25 07:23:50 +0100

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -6,12 +6,12 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
 
 /**
- * merge_subarr - Sort a subarray of integers.
+ * merge_subarr - Sort a subarray of integers
  * @subarr: A subarray of an array of integers to sort.
- * @buff: A buffer to store the sorted subarray.
- * @front: The front index of the array.
- * @mid: The middle index of the array.
- * @back: The back index of the array.
+ * @buff: A buffer to store the sorted subarray
+ * @front: The front index of the array
+ * @mid: The middle index of the array
+ * @back: The back index of the array
  */
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 		size_t back)
