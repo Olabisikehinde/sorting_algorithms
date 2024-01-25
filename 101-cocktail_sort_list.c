cb3f6328b3b4b88df84f94ccaac0ad33cc683828
Patrick christopher <141264544+darlingdiamond01@users.noreply.github.com>
2024-01-25 07:22:36 +0100

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -6,7 +6,7 @@ void cocktail_sort_list(listint_t **list);
 
 /**
  * swap_node_ahead - Swap a node in a listint_t doubly-linked list
- *                   list of integers with the node ahead of it.
+ *                   list of integers with the node ahead of it
  * @list: A pointer to the head of a doubly-linked list of integers.
  * @tail: A pointer to the tail of the doubly-linked list.
  * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
