047a9bbf32d070f5f59422eda81f578d06362cbe
Patrick christopher <141264544+darlingdiamond01@users.noreply.github.com>
2024-01-25 07:19:32 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap_ints - Swap two integers in an array
+ * @a: The first integer to swap
+ * @b: The second integer to swap
  */
 void swap_ints(int *a, int *b)
 {
